f4f1e8c2784d1b07daf8c2e88ca3e378b1158bb4
Raunak Kumar <raunakkr.saha@gmail.com>
2025-01-20 15:08:00 +0530

    objects in arr

---
+++ 5_object/p2_obj-in-arr.js
@@ -0,0 +1,22 @@
+const arr = [
+    {name: "Chhaya", age: 3},
+    {name: "Shanaya", age: 3},
+    {name: "Chulbul", age: 3},
+]
+
+for(let i=0; i<arr.length; i++){
+    console.log(arr[i].name);
+    
+}
+
+
+// arr.forEach((obj)=>{
+//     console.log(obj);
+// })
+
+//OUTPUT
+/**
+{ name: 'Chhaya', age: 3 }
+{ name: 'Shanaya', age: 3 }
+{ name: 'Chulbul', age: 3 }
+ */
